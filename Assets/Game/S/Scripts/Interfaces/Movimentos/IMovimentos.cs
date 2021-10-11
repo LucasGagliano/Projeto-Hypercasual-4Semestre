72cc634183fdd189c7eb663181686175f257240c
Lucas Martin <31822176+LucasGagliano@users.noreply.github.com>
2021-10-11 19:59:11 -0300

    Modificações / 11-10-2021
    As modificações incluem:
    - Adição da interface de movimentos
    - Adição do script de movimentação pendular
    - Adição do script de movimentação de desprender

---
+++ Assets/Game/S/Scripts/Interfaces/Movimentos/IMovimentos.cs
@@ -0,0 +1,21 @@
+namespace Game.S.Scripts.Interfaces
+{
+    using UnityEngine;
+
+    interface IMovimentos
+    {
+        #region Métodos
+    
+        #region Métodos Personalizados
+    
+        #region Métodos Públicos
+    
+        public void Acao(Rigidbody2D _rigidbody2D, float force);
+    
+        #endregion
+    
+        #endregion
+    
+        #endregion
+    }
+}
