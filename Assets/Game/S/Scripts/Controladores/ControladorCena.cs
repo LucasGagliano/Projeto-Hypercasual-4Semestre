a7b4ccf4a491119ce5fced0ccd5359d89d4cf62d
Lucas Martin <31822176+LucasGagliano@users.noreply.github.com>
2021-10-13 05:24:30 -0300

    Modificações / 13-10-2021
    As modificações incluem:
    - Adição de novos métodos nos controladores selecionados

--- Assets/Game/S/Scripts/Controladores/ControladorCena.cs
+++ Assets/Game/S/Scripts/Controladores/ControladorCena.cs
@@ -1,54 +1,17 @@
 namespace Game.S.Scripts.Controladores
 {
     using UnityEngine;
-
+    using UnityEngine.SceneManagement;
+    using Sistema;
+    
     public class ControladorCena : MonoBehaviour
     {
-        #region Variáveis
-
-        #region Variáveis Privadas
-
-        [SerializeField] [Tooltip("O uamento que os objetos selecionados sofreram em sua posição Y.")] private float aumentoVertical;
-        [SerializeField] [Tooltip("Os objetos que sofreram um aumento em sua posição Y.")] private GameObject[] objetosParaSubir;
-        private bool[] _encontrouObjetosParaSubir;
-
-        #endregion
-        
-        #endregion
-
-        #region Métodos
-
-        #region Métodos da Unity
-
-        #region Métodos Privados
-        
-        private void Start()
+        public void CarregarCena(string nome)
         {
-            _encontrouObjetosParaSubir = new bool[objetosParaSubir.Length];
-        
-            for (var i = 0; i < objetosParaSubir.Length; i++)
-                _encontrouObjetosParaSubir[i] = objetosParaSubir[i] != null;
+            SceneManager.LoadScene(nome, LoadSceneMode.Single);
+            DontDestroyOnLoad(InformacoesGerais.MusicaObjeto);
         }
-        
-        #endregion
-
-        #endregion
-
-        #region Métodos Personalizados
-
-        #region Métodos Públicos
-        
-        public void SubirObjetos()
-        {
-            for (var i = 0; i < objetosParaSubir.Length; i++)
-                if (_encontrouObjetosParaSubir[i])
-                    objetosParaSubir[i].transform.Translate(0, aumentoVertical * Time.deltaTime, 0);
-        }
-        
-        #endregion
-
-        #endregion
-
-        #endregion
     }
 }
+
+
